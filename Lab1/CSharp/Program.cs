0dafc2da2e817021dcbe31c01baf11351308f8d1
Mykyta Kovalenko <nikkv.ottf@gmail.com>
2022-09-18 16:39:19 +0200

    lab1: + c# methods

---
+++ Lab1/CSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+static List<T?> Singleton<T>(T? item) => new() { item };
+
+static bool Null<T>(List<T?>? list) => list?.Count == 0;
+
+static List<T?> Snoc<T>(List<T?> list, T? item) => new(list) { item };
+
+static int Length<T>(List<T?> list)
+{
+    int length = 0;
+
+    foreach (var _ in CollectionsMarshal.AsSpan(list))
+        length++;
+
+    return length;
+}
