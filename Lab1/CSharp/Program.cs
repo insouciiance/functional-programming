aeafb2678c614992732aeac8116db5f524cc1d0e
Mykyta Kovalenko <nikkv.ottf@gmail.com>
2022-09-28 17:01:54 +0200

    lab1: CSharp changes

--- Lab1/CSharp/Program.cs
+++ Lab1/CSharp/Program.cs
@@ -2,7 +2,7 @@
 
 static List<T?> Singleton<T>(T? item) => new() { item };
 
-static bool Null<T>(List<T?>? list) => list?.Count == 0;
+static bool Null<T>(List<T?>? list) => list is null or { Count: 0 };
 
 static List<T?> Snoc<T>(List<T?> list, T? item) => new(list) { item };
 
@@ -15,3 +15,14 @@ static int Length<T>(List<T?> list)
 
     return length;
 }
+
+Console.WriteLine($"[{string.Join(", ", Singleton("hi"))}]");   
+
+Console.WriteLine(Null<string>(new() { "a" }));
+Console.WriteLine(Null<object>(null));
+
+Console.WriteLine($"[{string.Join(", ", Snoc(new() { "a" }, "b"))}]");
+Console.WriteLine($"[{string.Join(", ", Snoc(new(), "b"))}]");
+
+Console.WriteLine(Length(new List<string?>() { "a", "b" }));
+Console.WriteLine(Length(new List<object?>()));
